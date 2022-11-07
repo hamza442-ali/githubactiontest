a8507b17e45831d5f44655212e7d9ca2a3a4aa8e
hamza442-ali <71401547+hamza442-ali@users.noreply.github.com>
2022-11-07 15:24:18 +0500

    testing

--- frontend/src/Views/searchNav.js
+++ frontend/src/Views/searchNav.js
@@ -14,7 +14,7 @@ export const NavSearch=()=>{
         <nav> 
         <div class="sidebar-button">
           <i class='bx bx-menu sidebarBtn'></i>
-          <span class="dashboard">jkvtigb</span>
+          <span class="dashboard">Buyer Portal</span>
           
         </div>
   
